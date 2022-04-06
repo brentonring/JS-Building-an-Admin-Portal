281c58b89ed8f3d4c43f984b30d10cd56f977ff8
Brenton Ring <brentonring@Brentons-Air.lan>
2022-04-05 21:36:44 -0400

    finished activity

--- public/index.js
+++ public/index.js
@@ -1,9 +1,22 @@
 async function main() {
 
-    let response = await fetch('http://localhost:3001/listBooks')
+    let response = await fetch('http://localhost:3001/listBooks', {
+    method: 'GET',
+});
+
     let books = await response.json()
+    console.log(books);
 
     books.forEach(renderBook)
+
+    await fetch('http://localhost:3001/updateBook', {
+        method: 'PATCH',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            'id': 3,
+            'title': 'The Legends of Arathrae'
+        })
+    })
 }
 
 function renderBook(book) {
